983f3c3021cd27d4c9f19d22835b687735e84c1b
jw791729 <43044257+jw791729@users.noreply.github.com>
2018-10-29 10:29:10 -0700

    Add files via upload

---
+++ NewBostonVideos/NewBoston10-1029/IfStatement.java
@@ -0,0 +1,25 @@
+
+/**
+ * TheNewBoston video #10
+ * If Statements
+ * Jason Wheaton
+ * 10-29
+ */
+class IfStatement{
+    
+    public static void main(){
+       
+        int test = 11;
+       
+       if (test > 6){
+           
+           System.out.println("yes");
+           
+       
+        }else{
+           
+            System.out.println("This is else");
+            
+}
+}
+}

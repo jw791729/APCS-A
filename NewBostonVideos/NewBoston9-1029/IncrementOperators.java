dce93f51b96597e14b4456c115ef86ae4dc72aa6
jw791729 <43044257+jw791729@users.noreply.github.com>
2018-10-29 10:18:32 -0700

    Add files via upload

---
+++ NewBostonVideos/NewBoston9-1029/IncrementOperators.java
@@ -0,0 +1,24 @@
+
+/**
+ * NewBoston Video #9
+ *
+ * Jason Wheaton
+ * 10-29
+ */
+import java.util.Scanner;
+class IncrementOperators{
+    
+    public static void main(String args[]){
+        
+        Scanner bobby = new Scanner(System.in);
+        
+        int tuna = 23;
+        int bass = 17;
+        tuna *= 4;
+        System.out.println(--bass);
+        System.out.println(tuna);
+        
+
+    
+    }
+}

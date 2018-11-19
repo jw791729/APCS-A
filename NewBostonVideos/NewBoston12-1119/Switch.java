661657cea128bc5ff31468b72adedc368c2b6c89
jw791729 <43044257+jw791729@users.noreply.github.com>
2018-11-19 09:47:39 -0800

    Add files via upload

---
+++ NewBostonVideos/NewBoston12-1119/Switch.java
@@ -0,0 +1,35 @@
+
+/**
+ * NewBoston video #12
+ *
+ * Jason Wheaton
+ * Version 11-19
+ */
+class Switch
+{
+    public static void main(){
+        
+        int age;
+        age = 2;
+        
+        switch (age) {
+        case 1:
+            System.out.println("You can work in a sweat shop.");
+            break;
+        case 2:
+            System.out.println("You can manage a sweat shop.");
+            break;
+        case 3:
+            System.out.println("You can get this bread.");
+            break;
+        case 4:
+            System.out.println("You can eat bread.");
+            break;
+        default:
+            System.out.println("Who is you bruh.");
+        break;
+        
+        
+    }
+}
+}

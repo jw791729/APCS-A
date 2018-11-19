26c9b04726deffba719ddfda997d2c73a6bba5bc
jw791729 <43044257+jw791729@users.noreply.github.com>
2018-11-19 09:55:25 -0800

    Add files via upload

---
+++ NewBostonVideos/NewBoston13-1119/WhileLoop.java
@@ -0,0 +1,17 @@
+
+/**
+ * NewBoston video #13
+ *
+ * Jason Wheaton
+ * Version 11-19
+ */
+class WhileLoop
+{
+    public static void main(){
+       int counter = 0;
+       while (counter < 42.0) {
+           System.out.println(counter);
+           counter ++;
+}
+}
+}

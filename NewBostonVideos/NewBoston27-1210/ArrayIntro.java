8233ba2f04fcc3e1ee67323c7dad0b91728025f3
jw791729 <43044257+jw791729@users.noreply.github.com>
2018-12-12 09:43:47 -0800

    Add files via upload

---
+++ NewBostonVideos/NewBoston27-1210/ArrayIntro.java
@@ -0,0 +1,19 @@
+
+/**
+ * TheNewBoston video #27 : Introduction to Arrays
+ *
+ * Jason Wheaton
+ * 10 December 2018
+ */
+class ArrayIntro{
+    public static void main(String[] args) {
+        
+        int bahddie[]={1,3,6,8,10};
+        
+       
+        
+        System.out.println(bahddie[2]);
+    
+    
+    }
+}

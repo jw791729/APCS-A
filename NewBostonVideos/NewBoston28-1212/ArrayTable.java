c677670b809460f182d2e38fbd4830ad62eeb2de
jw791729 <43044257+jw791729@users.noreply.github.com>
2018-12-12 10:18:17 -0800

    Add files via upload

---
+++ NewBostonVideos/NewBoston28-1212/ArrayTable.java
@@ -0,0 +1,17 @@
+
+/**
+ * NewBoston #28 - Creating an Array Table
+ *
+ * Jason Wheaton
+ * 12 December 2018
+ */
+class ArrayTable{
+    public static void main(){
+        System.out.println("Index/tValue");
+        int bicbui[]={67,9,17,33,98};
+        for(int counter=0;counter<bicbui.length;counter++){
+            System.out.println(counter + "/t" + bicbui[counter]);
+        
+}
+}
+}
